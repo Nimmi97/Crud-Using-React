a4c2cfba70b278526ccd38e11eb27542129623ec
Nimmi Walia <nwalia@rippling.com>
2022-09-13 19:59:10 +0530

    changed file structure

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const AddUser = ({ onAdd }) => {
 
-const handleOnSubmit = (e) => {
-    e.preventDefault();
-    onAdd(e.target.name.value,e.target.email.value);
-    e.target.name.value = "";
-    e.target.email.value = "";
-}
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if(e.target.name.value && e.target.email.value !== ''){
+            onAdd(e.target.name.value,e.target.email.value);
+            e.target.name.value = "";
+            e.target.email.value = "";
+
+        }
+       
+    }
 
   return (
     <div>
