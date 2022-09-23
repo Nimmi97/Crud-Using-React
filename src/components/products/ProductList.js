5793353566c0132bd86aa5ac4f46397effa99359
Nimmi Walia <nwalia@rippling.com>
2022-09-23 10:16:22 +0530

    Added navigation bar and react router and stack view for product list data.

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import './productList.css';
+import styled from "styled-components";
 
-function ProductList({ title, brand, description, rating, image }) {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 280px;
+  box-shadow: 0 3px 10px 0 #aaa;
+  cursor: pointer;
+`;
+
+const ProductName = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const ProductContainer = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding: 30px;
+gap: 25px;
+justify-content: space-evenly;;
+`;
+
+function ProductList({ data }) {
     return (
-        <div className='card'>
-            <div>
-                <img src={image} alt='image loading' width="250px" height="200px" />
-                <ul >
-                    <li>Modal: {title}</li>
-                    <li> Brand: {brand}</li>
-                    <li> Discription: {description}</li>
-                    <li>Ratings: {rating}</li>
-                </ul>
-            </div>
-            {/* <div className="container"> */}
+        <ProductContainer>{
+            data?.map(items => <Container><img src={items.thumbnail} alt='image loading' width="250px" height="200px" />
+                <ProductName>Model: {items.title}</ProductName>
+                <ProductName> Brand:{items.brand}</ProductName>
+                <ProductName>Discription: {items.description}</ProductName>
+                <ProductName>Ratings: {items.rating}</ProductName></Container>)
+        }</ProductContainer>
 
-            {/* </div> */}
-        </div>
+
     )
 }
 
