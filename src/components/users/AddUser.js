cd2a028e2ad8c4756a6e91a74d46c7a4ab397b5f
Nimmi Walia <nwalia@rippling.com>
2022-09-25 21:13:51 +0530

    Added colored theme to the toast container and configuired husky for the formatting.

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './user.css';
 
 const AddUser = ({ onAdd }) => {
@@ -7,16 +7,30 @@ const AddUser = ({ onAdd }) => {
     e.preventDefault();
     onAdd(userData.name, userData.email);
     setUserData({ name: '', email: '' });
-
-  }
+  };
 
   return (
     <div className="formContainer">
       <form onSubmit={handleOnSubmit}>
         <h2>Add New User</h2>
-        <input placeholder="Name" name="name" value={userData.name} onChange={(e) => setUserData({ name: e.target.value })} required />
-        <input placeholder="Email" name="email" value={userData.email} onChange={(e) => setUserData({ email: e.target.value })} required type="email" />
-        <button className="form-button" onSubmit={handleOnSubmit}>Add</button>
+        <input
+          placeholder="Name"
+          name="name"
+          value={userData.name}
+          onChange={(e) => setUserData({ name: e.target.value })}
+          required
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          value={userData.email}
+          onChange={(e) => setUserData({ email: e.target.value })}
+          required
+          type="email"
+        />
+        <button className="form-button" onSubmit={handleOnSubmit}>
+          Add
+        </button>
       </form>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
-
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-
