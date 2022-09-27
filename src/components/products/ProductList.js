e82c9798992cefa0a94a3fa95af75b6facd67007
Nimmi Walia <nwalia@rippling.com>
2022-09-27 22:18:58 +0530

    Added search bar in product list component to filter out the products accorting to the text in the search bar

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import styled from "styled-components";
-
+import React, { useRef } from 'react';
+import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,26 +15,52 @@ const ProductName = styled.span`
   text-overflow: ellipsis;
 `;
 const ProductContainer = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-padding: 30px;
-gap: 25px;
-justify-content: space-evenly;;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly; ;
+`;
+const InputConatainer = styled.div`
+  display: flex;
+`;
+const Button = styled.button`
+  margin: 5px;
+  padding: 5px;
+  background: linear-gradient(to left, #1cb5e0, #000046);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  color: white;
+  border-radius: 5px;
 `;
 
-function ProductList({ data }) {
-    return (
-        <ProductContainer>{
-            data?.map(items => <Container><img src={items.thumbnail} alt='image loading' width="250px" height="200px" />
-                <ProductName>Model: {items.title}</ProductName>
-                <ProductName> Brand:{items.brand}</ProductName>
-                <ProductName>Discription: {items.description}</ProductName>
-                <ProductName>Ratings: {items.rating}</ProductName></Container>)
-        }</ProductContainer>
+function ProductList({ data, searchQuery, inputHandler }) {
+  const inputRef = useRef('');
+  const getinputValue = () => {
+    inputHandler(inputRef.current.value);
+  };
 
+  return (
+    <>
+      <InputConatainer>
+        {' '}
+        <input value={searchQuery} onChange={getinputValue} ref={inputRef} />
+        <Button onClick={getinputValue}>Search</Button>
+      </InputConatainer>
 
-    )
+      <ProductContainer>
+        {data?.map((items) => (
+          <Container key={items.id}>
+            <img src={items.thumbnail} alt="image loading" width="250px" height="200px" />
+            <ProductName>Model: {items.title}</ProductName>
+            <ProductName> Brand:{items.brand}</ProductName>
+            <ProductName>Discription: {items.description}</ProductName>
+            <ProductName>Ratings: {items.rating}</ProductName>
+          </Container>
+        ))}
+      </ProductContainer>
+    </>
+  );
 }
 
-export default ProductList
+export default ProductList;
